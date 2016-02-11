d814cb5e11d30e37ffaab8b5b644618cc8a53adc
Amanda <amanda@Amandas-MacBook-Air-3.local>
2016-02-11 13:02:48 -0600

    update config; use ambiance theme and add codemirror plugin

--- config.js
+++ config.js
@@ -4,69 +4,113 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-	// %REMOVE_START%
-	config.plugins =
-		'about,' +
-		'a11yhelp,' +
-		'basicstyles,' +
-		'bidi,' +
-		'blockquote,' +
-		'clipboard,' +
-		'colorbutton,' +
-		'colordialog,' +
-		'contextmenu,' +
-		'dialogadvtab,' +
-		'div,' +
-		'elementspath,' +
-		'enterkey,' +
-		'entities,' +
-		'filebrowser,' +
-		'find,' +
-		'flash,' +
-		'floatingspace,' +
-		'font,' +
-		'format,' +
-		'forms,' +
-		'horizontalrule,' +
-		'htmlwriter,' +
-		'image,' +
-		'iframe,' +
-		'indentlist,' +
-		'indentblock,' +
-		'justify,' +
-		'language,' +
-		'link,' +
-		'list,' +
-		'liststyle,' +
-		'magicline,' +
-		'maximize,' +
-		'newpage,' +
-		'pagebreak,' +
-		'pastefromword,' +
-		'pastetext,' +
-		'preview,' +
-		'print,' +
-		'removeformat,' +
-		'resize,' +
-		'save,' +
-		'selectall,' +
-		'showblocks,' +
-		'showborders,' +
-		'smiley,' +
-		'sourcearea,' +
-		'specialchar,' +
-		'stylescombo,' +
-		'tab,' +
-		'table,' +
-		'tabletools,' +
-		'templates,' +
-		'toolbar,' +
-		'undo,' +
-		'wysiwygarea';
-	// %REMOVE_END%
+	// Define changes to default configuration here.
+	// For complete reference see:
+	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+	config.allowedContent = true;
+	// The toolbar groups arrangement, optimized for two toolbar rows.
+	config.toolbarGroups = [
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'links' },
+		{ name: 'insert' },
+		{ name: 'forms' },
+		{ name: 'tools' },
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'others' },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		{ name: 'styles' },
+		{ name: 'colors' },
+		{ name: 'about' }
+	];
+
+	// Remove some buttons provided by the standard plugins, which are
+	// not needed in the Standard(s) toolbar.
+	config.removeButtons = 'Underline,Subscript,Superscript';
+
+	// Set the most common block elements.
+	config.format_tags = 'p;h1;h2;h3;pre';
+
+	// Simplify the dialog windows.
+	config.removeDialogTabs = 'image:advanced;link:advanced';
+
+	config.codemirror = {
+
+	    // Whether or not you want Brackets to automatically close themselves
+	    autoCloseBrackets: true,
+
+	     // Whether or not you want tags to automatically close themselves
+	    autoCloseTags: true,
+
+	     // Whether or not to automatically format code should be done when the editor is loaded
+	    autoFormatOnStart: true,
+
+	    // Whether or not to automatically format code which has just been uncommented
+	    autoFormatOnUncomment: true,
+
+	    // Whether or not to continue a comment when you press Enter inside a comment block
+	    continueComments: true,
+
+	     // Whether or not you wish to enable code folding (requires 'lineNumbers' to be set to 'true')
+	    enableCodeFolding: true,
+
+	    // Whether or not to enable code formatting
+	    enableCodeFormatting: true,
+
+	    // Whether or not to enable search tools, CTRL+F (Find), CTRL+SHIFT+F (Replace), CTRL+SHIFT+R (Replace All), CTRL+G (Find Next), CTRL+SHIFT+G (Find Previous)
+	    enableSearchTools: true,
+
+	    // Whether or not to highlight all matches of current word/selection
+	    highlightMatches: true,
+
+	     // Whether, when indenting, the first N*tabSize spaces should be replaced by N tabs
+	    indentWithTabs: false,
+
+	     // Whether or not you want to show line numbers
+	    lineNumbers: true,
+
+	    // Whether or not you want to use line wrapping
+	    lineWrapping: true,
+
+	     // Define the language specific mode 'htmlmixed' for html  including (css, xml, javascript), 'application/x-httpd-php' for php mode including html, or 'text/javascript' for using java script only
+	    mode: 'htmlmixed',
+
+	    // Whether or not you want to highlight matching braces
+	    matchBrackets: true,
+
+	    // Whether or not you want to highlight matching tags
+	    matchTags: true,
+
+	    // Whether or not to show the showAutoCompleteButton   button on the toolbar
+	    showAutoCompleteButton: true,
+
+	     // Whether or not to show the comment button on the toolbar
+	    showCommentButton: true,
+
+	    // Whether or not to show the format button on the toolbar
+	    showFormatButton: true,
+
+	     // Whether or not to show the search Code button on the toolbar
+	    showSearchButton: true,
+
+	     // Whether or not to show Trailing Spaces
+	    showTrailingSpace: true,
+
+	    // Whether or not to show the uncomment button on the toolbar
+	    showUncommentButton: true,
+
+	     // Whether or not to highlight the currently active line
+	    styleActiveLine: true,
+
+	     // Set this to the theme you wish to use (codemirror themes)
+	    theme: 'ambiance',
+
+	    // "Whether or not to use Beautify for auto formatting
+	    useBeautify: false
+	};
 };
+
 
 // %LEAVE_UNMINIFIED% %REMOVE_LINE%
